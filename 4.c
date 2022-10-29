9646b631cfb21f0b0154f795a3eb949909a23f21
karanvirghotra <115352184+karanvirghotra@users.noreply.github.com>
2022-10-29 20:16:31 +0530

    Add files via upload

---
+++ 4.c
@@ -0,0 +1,11 @@
+/*Write a program to print the ASCII value of a character.*/
+#include<stdio.h>
+int main()
+{
+	char c;
+	
+	printf("Enter character: ");
+    scanf("%c",&c);
+    printf("/nASCII value: %d",c);
+	return 0;
+}
